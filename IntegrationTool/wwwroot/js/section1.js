5e6b32b6bca035181c3552bf3ac6e7e59f2e70ac
John Wendell Unabia <wendell.unabia@gmail.com>
2021-06-21 02:43:23 +0800

    UI Changes

--- IntegrationTool/wwwroot/js/section1.js
+++ IntegrationTool/wwwroot/js/section1.js
@@ -15,14 +15,14 @@ $(document).ready(function () {
     $('.forUpdatedis').prop("disabled", true);
 
     $('.btnEdit').click(function () {
-        $(this).parent().find('.forView').hide();
-        $(this).parent().find('.forUpdate').show();
+        $(this).parent().parent().find('.forView').hide();
+        $(this).parent().parent().find('.forUpdate').show();
         
 
-        $(this).parent().parent().find('.col-sm-5').find('.forView').hide();
-        $(this).parent().parent().find('.col-sm-5').find('.forUpdate').show();
-        $(this).parent().parent().find('.col-sm-5').find('.forUpdatedis').prop("disabled", false);
-        $(this).parent().parent().find('.col-sm-8').find('.forUpdatedis').prop("disabled", false);
+        $(this).parent().parent().parent().find('.col-sm-5').find('.forView').hide();
+        $(this).parent().parent().parent().find('.col-sm-5').find('.forUpdate').show();
+        $(this).parent().parent().parent().find('.col-sm-5').find('.forUpdatedis').prop("disabled", false);
+        $(this).parent().parent().parent().find('.col-sm-8').find('.forUpdatedis').prop("disabled", false);
         //$('#lblBill').hide();
         //$('#txtBill').show();
         //$('#btnSaveBill').show();
@@ -31,14 +31,14 @@ $(document).ready(function () {
 
 
     $('.btnSave').click(function () {
-        $(this).parent().find('.forView').show();
-        $(this).parent().find('.forUpdate').hide();
+        $(this).parent().parent().find('.forView').show();
+        $(this).parent().parent().find('.forUpdate').hide();
 
 
-        $(this).parent().parent().find('.col-sm-5').find('.forView').show();
-        $(this).parent().parent().find('.col-sm-5').find('.forUpdate').hide();
-        $(this).parent().parent().find('.col-sm-5').find('.forUpdatedis').prop('disabled', true);
-        $(this).parent().parent().find('.col-sm-8').find('.forUpdatedis').prop('disabled', true);
+        $(this).parent().parent().parent().find('.col-sm-5').find('.forView').show();
+        $(this).parent().parent().parent().find('.col-sm-5').find('.forUpdate').hide();
+        $(this).parent().parent().parent().find('.col-sm-5').find('.forUpdatedis').prop('disabled', true);
+        $(this).parent().parent().parent().find('.col-sm-8').find('.forUpdatedis').prop('disabled', true);
     });
 
 
@@ -47,9 +47,9 @@ $(document).ready(function () {
         $(this).parent().find('.forUpdate').hide();
 
 
-        $(this).parent().parent().find('.col-sm-5').find('.forView').show();
-        $(this).parent().parent().find('.col-sm-5').find('.forUpdate').hide();
-        $(this).parent().parent().find('.col-sm-5').find('.forUpdatedis').prop('disabled', true);
-        $(this).parent().parent().find('.col-sm-8').find('.forUpdatedis').prop('disabled', true);
+        $(this).parent().parent().parent().find('.col-sm-5').find('.forView').show();
+        $(this).parent().parent().parent().find('.col-sm-5').find('.forUpdate').hide();
+        $(this).parent().parent().parent().find('.col-sm-5').find('.forUpdatedis').prop('disabled', true);
+        $(this).parent().parent().parent().find('.col-sm-8').find('.forUpdatedis').prop('disabled', true);
     });
 });
